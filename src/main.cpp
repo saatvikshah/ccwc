a4bf75b709441ff5de50d68deefed1406918c994
Saatvik Shah <saatvikshah1994@gmail.com>
2025-05-03 21:58:11 +0200

    Setup `-c` for counting bytes

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,31 @@
-#include <iostream>
+#include <tclap/CmdLine.h>
 
-auto main() -> int {
-    std::cout << "Hello, World!\n";
+#include <filesystem>
+#include <iostream>
+#include <span>
+
+namespace fs = std::filesystem;
+
+auto main(int anArgc, char* anArgv[]) -> int {
+    try {
+        TCLAP::CmdLine myCmd("Count bytes in file", ' ', "0.1");  // NOLINT
+
+        TCLAP::SwitchArg myBytesOnlySwitch("c", "bytes", "Only count bytes", myCmd, false);
+        TCLAP::UnlabeledValueArg<std::string> myFilenameArg("filename", "Input file", true, "",
+                                                            "filename");
+        myCmd.add(myFilenameArg);
+        myCmd.parse(anArgc, anArgv);
+
+        bool myIsBytesOnly = myBytesOnlySwitch.getValue();
+        const auto& myFilename = myFilenameArg.getValue();
+
+        if (myIsBytesOnly) {
+            auto myFileSize = fs::file_size(myFilename);
+            std::cout << myFileSize << " " << myFilename << '\n';
+        }
+    } catch (const std::exception& anError) {
+        std::cerr << "Error: " << anError.what() << '\n';
+        return 1;
+    }
     return 0;
 }
